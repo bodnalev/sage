84ba1ae030222b08fff67a04d0d34babfa62229d
EmptySet <bodnalev@gmail.com>
2024-07-19 14:13:50 +0100

    flagmatic code
    python 2to3 changes, import changes, random c library removes
    
    still buggy

---
+++ src/sage/combinat/flagmatic/all.py
@@ -0,0 +1,43 @@
+"""
+
+flagmatic 2
+
+Copyright (c) 2012, E. R. Vaughan. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1) Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2) Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+
+from sage.combinat.flagmatic.flag import *
+from sage.combinat.flagmatic.hypergraph_flag import *
+from sage.combinat.flagmatic.three_graph_flag import *
+from sage.combinat.flagmatic.graph_flag import *
+from sage.combinat.flagmatic.oriented_graph_flag import *
+from sage.combinat.flagmatic.multigraph_flag import *
+
+from sage.combinat.flagmatic.problem import *
+from sage.combinat.flagmatic.axioms_problem import *
+
+from sage.combinat.flagmatic.construction import *
+from sage.combinat.flagmatic.blowup_construction import *
+from sage.combinat.flagmatic.random_graph_construction import *
+from sage.combinat.flagmatic.random_tournament_construction import *
